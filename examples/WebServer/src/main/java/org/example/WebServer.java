9802a73e1ec7e5ce0610d1b2d5fb613110c6c853
Makhmut <mahmudlyder@gmail.com>
2022-10-21 17:55:34 +0600

    feat: run from thread

--- examples/WebServer/src/main/java/org/example/WebServer.java
+++ examples/WebServer/src/main/java/org/example/WebServer.java
@@ -3,43 +3,38 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+
 /**
  * Simple web server.
  */
 public class WebServer {
-    public static void main(String[] args) {
-        // Port number for http request
-        int port = args.length > 1 ? Integer.parseInt(args[1]) : 9980;
-        // The maximum queue length for incoming connection
-        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;;
 
-        try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
-            System.out.println("Web Server is starting up, listening at port " + port + ".");
-            System.out.println("You can access http://localhost:" + port + " now.");
+  public static void main(String[] args) {
+    // Port number for http request
+    int port = args.length > 1 ? Integer.parseInt(args[1]) : 9980;
+    // int numOfThreads = args.length > 2 ? Integer.parseInt(args[2]) : 4;
+    // The maximum queue length for incoming connection
+    int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;
+    
 
-            while (true) {
-                // Make the server socket wait for the next client request
-                Socket socket = serverSocket.accept();
-                System.out.println("Got connection!");
+    try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
+      System.out.println("Web Server is starting up, listening at port " + port + ".");
+      System.out.println("You can access http://localhost:" + port + " now.");
 
-                // To read input from the client
-                BufferedReader input = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+      while (true) {
+        // Make the server socket wait for the next client request
+        Socket socket = serverSocket.accept();
+        System.out.println("Got connection!");
 
-                // Get request
-                HttpRequest request = HttpRequest.parse(input);
-
-                // Process request
-                Processor proc = new Processor(socket, request);
-                proc.process();
-            }
-        }
-        catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        finally {
-            System.out.println("Server has been shutdown!");
-        }
+        ClientHandler clientSock 
+                    = new ClientHandler(socket); 
+        
+        new Thread(clientSock).start();
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    } finally {
+      System.out.println("Server has been shutdown!");
     }
+  }
 }
